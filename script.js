60286a92ea61bbff53c8b29b67d17cb614cf33a0
tod619 <trevorodwyer619@hotmail.com>
2021-06-07 15:30:32 +0100

    add animation to svg to decrease border on tick in script.js file

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const durationInput = document.getElementById('duration')
 const startButton = document.getElementById('start')
 const pauseButton = document.getElementById('pause')
+const circle = document.querySelector('circle')
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI
+circle.setAttribute('stroke-dasharray', perimeter)
+
+let currentOffset = 0
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(){
         console.log('timer started')
     },
     onTick(){
-        console.log('timer just ticked down')
+        circle.setAttribute('stroke-dashoffset', currentOffset)
+        currentOffset = currentOffset - 50
     },
     onComplete(){
         console.log('timer is completed')
